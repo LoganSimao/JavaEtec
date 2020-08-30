1639ec7130aa0547f44f7a4c09a62f42eca4d429
LoganSimao <66693243+LoganSimao@users.noreply.github.com>
2020-08-30 17:05:48 -0300

    Multiplicador

--- src/Multiplicador.java
+++ src/Multiplicador.java
@@ -7,12 +7,12 @@ public class Multiplicador {
     public static void Mult(){
         // declaração de variaveis para não ter problema com as memoria
         double n1, n2, total=0;
-        String sistem_input="";
+        String system_input="";
 
-        sistem_input = JOptionPane.showInputDialog(null, "first number: ");
-        n1= Double.parseDouble(sistem_input);
-        sistem_input = JOptionPane.showInputDialog(null, "second number: ");
-        n2 = Double.parseDouble(sistem_input);
+        system_input = JOptionPane.showInputDialog(null, "first number: ");
+        n1= Double.parseDouble(system_input);
+        system_input = JOptionPane.showInputDialog(null, "second number: ");
+        n2 = Double.parseDouble(system_input);
 
         //multiplicação dos valores
         total = n1 * n2;
